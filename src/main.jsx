a15c10a5b0cad612a0900b577e7a39fa71dcba60
Yassine Benhamzah <yassine.bhmz@hotmail.com>
2026-03-12 16:33:42 +0000

    feat: initialize React Vite project

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import { ToastProvider } from './context/ToastContext'
+import router from './router'
+import './index.css'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <ToastProvider>
+        <RouterProvider router={router} />
+      </ToastProvider>
+    </AuthProvider>
+  </React.StrictMode>,
+)
